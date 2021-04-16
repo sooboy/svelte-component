cfbac602dbb9ced2f5a3b0f2ae394b87343907c7
Ni <sooboy@foxmail.com>
2021-04-16 16:18:14 +0800

    delete

--- src/interface.ts
+++ src/interface.ts
@@ -1,10 +1,3 @@
-// export let bg: string;
-// export let thumb: string;
-// export let name: string = "我原谅你了";
-// export let money: string = "100";
-// export let tag: ItemTag;
-// export let id: string;
-
 export interface Itemer {
      bg:string;
      thumb:string;
